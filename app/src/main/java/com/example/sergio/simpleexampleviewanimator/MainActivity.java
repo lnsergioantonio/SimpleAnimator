31373019739106edf99edd67bb58cc993041d6a3
sergio <lnsergioantonio@hotmail.com>
2019-01-07 22:05:32 -0500

    init

---
+++ app/src/main/java/com/example/sergio/simpleexampleviewanimator/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.sergio.simpleexampleviewanimator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewAnimator;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ViewAnimator viewAnimator;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewAnimator = findViewById(R.id.viewAnimator);
+
+        findViewById(R.id.btnNext).setOnClickListener(this);
+        findViewById(R.id.btnPrevious).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnNext:
+                if(viewAnimator.getCurrentView().getId()!=R.id.three)
+                    viewAnimator.showNext();
+                break;
+            case R.id.btnPrevious:
+                if(viewAnimator.getCurrentView().getId()!=R.id.one)
+                    viewAnimator.showPrevious();
+                break;
+
+        }
+    }
+}
